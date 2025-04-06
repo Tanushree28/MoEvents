5d9955d6fc2471a135f138b0aaf2e919f26aec2e
Nischal Shakya <nischalshakya2015@gmail.com>
2025-04-07 00:11:23 +0545

    Add APIs for create, get and update

--- backend/app/routers/events.py
+++ backend/app/routers/events.py
@@ -4,13 +4,28 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from .. import schemas, crud
 from ..database import get_db
-from typing import List
 
-router = APIRouter(
-    prefix="/events",
-    tags=["Events"]
-)
+router = APIRouter(prefix="/events", tags=["Events"])
 
-@router.get("/", response_model=List[schemas.EventOut])
+
+@router.post("/", response_model=schemas.EventCreate)
+def create_event(
+    event: schemas.EventCreate,
+    db: Session = Depends(get_db),
+):
+    return crud.create_event(db, event)
+
+
+@router.get("/", response_model=list[schemas.EventRead])
 def read_events(db: Session = Depends(get_db)):
     return crud.get_all_events(db)
+
+
+@router.get("/{id}", response_model=schemas.EventRead)
+def get_event_by_id(id: int, db: Session = Depends(get_db)):
+    return crud.get_event_by_id(db, id)
+
+
+@router.put("/{id}", response_model=schemas.EventRead)
+def update_event(id: int, event: schemas.EventUpdate, db: Session = Depends(get_db)):
+    return crud.update_event_by_id(db, id, event)
